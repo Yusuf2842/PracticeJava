ec9eb827af68617ae5006f0cb9b148166067403f
Yusuf2842 <123114153+Yusuf2842@users.noreply.github.com>
2024-03-04 11:39:22 +0300

    Continued working

--- ATM/src/main/atmProject/atmClass/atmAccount.java
+++ ATM/src/main/atmProject/atmClass/atmAccount.java
@@ -2,9 +2,18 @@ package main.atmProject.atmClass;
 
 public class atmAccount {
     // Common properties and methods for all accounts
+    private double balance;
+
+    public void atmAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
 
     public void deposit(double amount) { //TODO: Figure out how to use this method
-        System.out.println(amount);
+        this.balance = amount;
+    }
+
+    public void test() {
+        System.out.println("Test line: " + this.balance);
     }
 
 }
--- ATM/src/test/atmProject/atmTest.java
+++ ATM/src/test/atmProject/atmTest.java
@@ -1,8 +1,13 @@
-package test;
+package test.atmProject;
 import main.atmProject.atmClass.*;
 
 public class atmTest {
     public static void main(String[] args) {
         //TODO: Figure out how to use the atmClass method
+        atmAccount myAccount = new atmAccount();
+
+        myAccount.test();
+        myAccount.deposit(100);
+        myAccount.test();
     }
 }
