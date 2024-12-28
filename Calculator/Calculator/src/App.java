1d52362f40d5551c1a8f1530cb435bdc66f72897
Yusuf2842 <123114153+Yusuf2842@users.noreply.github.com>
2024-12-28 16:45:00 -0500

    Still trying to figure out how to do Java

--- Calculator/Calculator/src/App.java
+++ Calculator/Calculator/src/App.java
@@ -1,12 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-        Scanner scanner = new Scanner(System.in); 
-
-        System.out.println("Calculate: "); //Print "Calculate"
-        int numberInput = scanner.nextInt(); // Scan for next int
-        System.out.println("The number is: " + numberInput); //Test line
+        
     }
 }
