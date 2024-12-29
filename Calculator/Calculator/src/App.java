d0d9df7248523853f21f8d672bd358b8425cf488
Yusuf2842 <123114153+Yusuf2842@users.noreply.github.com>
2024-12-29 15:54:45 -0500

    Kept working on it

--- Calculator/Calculator/src/App.java
+++ Calculator/Calculator/src/App.java
@@ -1,8 +1,26 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String input;
+
+        do {
+            System.out.println("Enter a number (Type 'Exit' to quit): "); //Asks user for input
+            input = scanner.nextLine(); //Scans for input
+
+            if (input.equalsIgnoreCase("Exit")) {
+                System.out.println("In the loop to break");
+                System.out.println("Goodbye!");
+                break;
+            } else {
+                System.out.println("The number is: " + input);
+                double[] numbers = new double[2];
+
+            }
+        } while (true);
+
+        scanner.close();
     }
 }
