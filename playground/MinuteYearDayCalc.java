a9d7d1393e4768e8bef76c6697addeab321b6109
Yusuf2842 <123114153+Yusuf2842@users.noreply.github.com>
2025-04-29 16:42:25 -0400

    I love you

---
+++ playground/MinuteYearDayCalc.java
@@ -0,0 +1,25 @@
+package playground;
+
+public class MinuteYearDayCalc {
+
+    public static void main(String[] args) {
+
+        printYearsAndDays(525600);
+        
+    }
+
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        long hours = (minutes / 60);
+        long days = (hours / 24);
+        long years = (days / 365);
+        long remainderDays = (days % 365);
+
+        System.out.println(minutes + " min = " + years + " y and " + remainderDays + " d");
+
+    }
+}
---
+++ playground/Sum3And5.java
@@ -0,0 +1,16 @@
+package playground;
+
+public class Sum3And5 {
+
+    public static void main(String[] args) {
+     
+        int sum = 0;
+        for (int i = 1; (i <= 1000); i++) {
+            if (((i % 3) == 0) && ((i % 5) == 0)) {
+                System.out.println("The number is " + i);
+                sum += i;
+                System.out.println(sum);
+            }
+        }
+    }
+}
