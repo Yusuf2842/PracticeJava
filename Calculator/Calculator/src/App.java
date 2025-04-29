9e14e6b212a0d36fa58a3dcac545796a15d643a1
Yusuf2842 <123114153+Yusuf2842@users.noreply.github.com>
2025-04-29 01:33:07 -0400

    Practiced Java

--- Calculator/Calculator/src/App.java
+++ Calculator/Calculator/src/App.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
@@ -7,17 +8,30 @@ public class App {
         String input;
 
         do {
-            System.out.println("Enter a number (Type 'Exit' to quit): "); //Asks user for input
+            System.out.println("Press Enter to Continue."); //Asks user for input and terminates if 'exit
+            System.out.println("Type 'Exit' to quit.");
             input = scanner.nextLine(); //Scans for input
 
-            if (input.equalsIgnoreCase("Exit")) {
+            if (input.equalsIgnoreCase("Exit")) { //Checks if user typed exit
                 System.out.println("In the loop to break");
                 System.out.println("Goodbye!");
                 break;
             } else {
-                System.out.println("The number is: " + input);
-                double[] numbers = new double[2];
+                double[] numbers = new double[2]; //Initialize double array
+                for (int i = 0; i < numbers.length; i++) { //For loop to add user input into the array.
+                    System.out.println("Enter first number: ");
+                    if (scanner.hasNextDouble()) {
+                        numbers[i] = scanner.nextDouble();
+                    } else {
+                        System.out.println("Invalid input, please enter a number.");
+                        scanner.next();
+                    }
+                    scanner.nextLine();
+                }
+                System.out.println("Array contents: " + Arrays.toString(numbers));
+                for (int i = 0; i < numbers.length; i++) { //TODO: Try to figure out how to use the two numbers in the array and apply an operator on them.
 
+                }
             }
         } while (true);
 
